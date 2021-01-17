a14bcb3085b7e299703509478dd47e591dccdc93
hayato iida <hayato.iida.0213@gmail.com>
2021-01-17 19:09:43 +0900

    fix

--- prototypes/category.lua
+++ prototypes/category.lua
@@ -42,7 +42,7 @@ data:extend(
             {
                 type = "item-subgroup",
                 name = "fni-item",
-                group = "food",
+                group = "fni",
                 order = "g-f",
             },
             {
