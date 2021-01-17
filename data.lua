73b22c3aa2e6fda1e2657a885cabb39448f66103
hayato iida <hayato.iida.0213@gmail.com>
2021-01-17 17:11:23 +0900

    add items

--- data.lua
+++ data.lua
@@ -1,5 +1,6 @@
 -- main data
 require("prototypes.item")
+require("prototypes.entity-item")
 require("prototypes.fluids")
 require("prototypes.recipe")
 require("prototypes.entity")
---
+++ prototypes/entity-item.lua
@@ -0,0 +1,13 @@
+data:extend({
+
+    {
+        type = "item",
+        name = "colony-1",
+        icon = "__factorio-not-included__/graphics/empty.png",
+        icon_size = 64,
+        order = "w-b-a",
+        place_result = "colony-1",
+        subgroup = "fni-colony",
+        stack_size = 10,
+    },
+})
