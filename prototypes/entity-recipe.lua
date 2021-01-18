16deb9df32f7d306ea5b159b8a849f43f81aad0c
hayato iida <hayato.iida.0213@gmail.com>
2021-01-18 14:32:38 +0900

    fix

--- prototypes/entity-recipe.lua
+++ prototypes/entity-recipe.lua
@@ -9,18 +9,18 @@ data:extend({
         subgroup = "fni-building",
         ingredients =
         {
-            {"iron-plate", 10},
-            {"copper-plate", 10},
-            {"electronic-circuit", 6},
-            {"iron-stick", 8},
-            {"iron-gear-wheel", 12},
+            { "iron-plate", 10 },
+            { "copper-plate", 10 },
+            { "electronic-circuit", 6 },
+            { "iron-stick", 8 },
+            { "iron-gear-wheel", 12 },
         },
-        results = "colony-1"
+        result = "colony-1"
     },
     {
         type = "recipe",
         name = "entity-farm-1",
-        results = "farm-1",
+        result = "farm-1",
         enabled = true,
         icon = "__factorio-not-included__/graphics/empty.png",
         icon_size = 32,
@@ -28,15 +28,15 @@ data:extend({
         subgroup = "fni-building",
         ingredients =
         {
-            {"copper-plate", 5},
-            {"electronic-circuit", 3},
-            {"iron-gear-wheel", 6},
+            { "copper-plate", 5 },
+            { "electronic-circuit", 3 },
+            { "iron-gear-wheel", 6 },
         },
     },
     {
         type = "recipe",
         name = "entity-coocker-1",
-        results = "coocker-1",
+        result = "coocker-1",
         enabled = true,
         icon = "__factorio-not-included__/graphics/empty.png",
         icon_size = 32,
@@ -44,15 +44,15 @@ data:extend({
         subgroup = "fni-building",
         ingredients =
         {
-            {"iron-plate", 5},
-            {"copper-plate", 3},
-            {"electronic-circuit", 6},
+            { "iron-plate", 5 },
+            { "copper-plate", 3 },
+            { "electronic-circuit", 6 },
         },
     },
     {
         type = "recipe",
         name = "entity-water-sieve-1",
-        results = "water-sieve-1",
+        result = "water-sieve-1",
         enabled = true,
         icon = "__factorio-not-included__/graphics/empty.png",
         icon_size = 32,
@@ -60,15 +60,15 @@ data:extend({
         subgroup = "fni-building",
         ingredients =
         {
-            {"iron-plate", 10},
-            {"copper-plate", 3},
-            {"electronic-circuit", 6},
+            { "iron-plate", 10 },
+            { "copper-plate", 3 },
+            { "electronic-circuit", 6 },
         },
     },
     {
         type = "recipe",
         name = "entity-compost-1",
-        results = "compost-1",
+        result = "compost-1",
         enabled = true,
         icon = "__factorio-not-included__/graphics/empty.png",
         icon_size = 32,
@@ -76,14 +76,14 @@ data:extend({
         subgroup = "fni-building",
         ingredients =
         {
-            {"iron-plate", 10},
-            {"electronic-circuit", 6},
+            { "iron-plate", 10 },
+            { "electronic-circuit", 6 },
         },
     },
     {
         type = "recipe",
         name = "entity-air-cleaner-1",
-        results = "air-cleaner-1",
+        result = "air-cleaner-1",
         enabled = true,
         icon = "__factorio-not-included__/graphics/empty.png",
         icon_size = 32,
@@ -91,15 +91,15 @@ data:extend({
         subgroup = "fni-building",
         ingredients =
         {
-            {"iron-plate", 10},
-            {"iron-gear-wheel", 6},
-            {"electronic-circuit", 6},
+            { "iron-plate", 10 },
+            { "iron-gear-wheel", 6 },
+            { "electronic-circuit", 6 },
         },
     },
     {
         type = "recipe",
         name = "entity-carbon-skimmer-1",
-        results = "carbon-skimmer-1",
+        result = "carbon-skimmer-1",
         enabled = true,
         icon = "__factorio-not-included__/graphics/empty.png",
         icon_size = 32,
@@ -107,15 +107,15 @@ data:extend({
         subgroup = "fni-building",
         ingredients =
         {
-            {"iron-plate", 10},
-            {"iron-gear-wheel", 6},
-            {"electronic-circuit", 6},
+            { "iron-plate", 10 },
+            { "iron-gear-wheel", 6 },
+            { "electronic-circuit", 6 },
         },
     },
     {
         type = "recipe",
         name = "entity-gas-pump-1",
-        results = "gas-pump-1",
+        result = "gas-pump-1",
         enabled = true,
         icon = "__factorio-not-included__/graphics/empty.png",
         icon_size = 32,
@@ -123,9 +123,9 @@ data:extend({
         subgroup = "fni-building",
         ingredients =
         {
-            {"iron-plate", 10},
-            {"iron-gear-wheel", 8},
-            {"electronic-circuit", 6},
+            { "iron-plate", 10 },
+            { "iron-gear-wheel", 8 },
+            { "electronic-circuit", 6 },
         },
     },
 })
