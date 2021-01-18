448df6afab20f069f258d39fb9e037fdb65a04ab
hayato iida <hayato.iida.0213@gmail.com>
2021-01-18 13:25:11 +0900

    fix

--- data.lua
+++ data.lua
@@ -1,6 +1,7 @@
 -- main data
 require("prototypes.item")
 require("prototypes.entity-item")
+require("prototypes.entity-recipe")
 require("prototypes.fluids")
 require("prototypes.recipe")
 require("prototypes.entity")
