4d3854ced28f6413edeafedbfe795d3268b6b8d7
hayato iida <hayato.iida.0213@gmail.com>
2021-01-24 00:39:39 +0900

    fix items

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -116,4 +116,22 @@ data:extend({
         subgroup = "fni-item",
         stack_size = 1,
     },
+    {
+        type = "item",
+        name = "slime",
+        icon = "__factorio-not-included__/graphics/slime/item/64.png",
+        icon_size = 64,
+        order = "w-b-a",
+        subgroup = "fni-item",
+        stack_size = 1,
+    },
+    {
+        type = "item",
+        name = "algae",
+        icon = "__factorio-not-included__/graphics/algae/item/64.png",
+        icon_size = 64,
+        order = "w-b-a",
+        subgroup = "fni-item",
+        stack_size = 1,
+    },
 })
